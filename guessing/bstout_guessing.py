d0f418db572cfa171837793f39927a0ed246aab6
Brian Stout <Brian Stout>
2016-10-14 19:00:55 -0400

    Final submit cause I got stuff to do, modified the while loop so it doesn't required a flag -derp- and modified the subsequent functions to be simpler because of this, did a final sweep for PEP8 errors

--- guessing/bstout_guessing.py
+++ guessing/bstout_guessing.py
@@ -2,103 +2,111 @@
 
 import random
 
+
 def comparenumbers(guess, randomNumber):
     """This function compares two values, determines if it is equivalent,
        greater than, or less than.  It then prints a string letting the
        player know which it is.
-       
+
        It returns a bool value of False if the values are equivalent which
        tells the program to stop running the loop because the player won
        """
 
     if guess == randomNumber:
-        return False
+        pass
     elif guess > randomNumber:
         print(guess, "is too high - please guess again", end="")
-        return True
     else:
         print(guess, "is too low  - please guess again", end="")
-        return True
+
 
 def comparenumberslie(guess, randomNumber):
-    """This function does the same thing as comparenumbers() but lies about the results
+    """This function does the same thing as comparenumbers() but lies about the
+       results
 
-       Additionally it returns a string stating which number it lied about so it
-       can be called later.  This function return both objects in a list instead
-       of a tuple to cut back on code lines and to make it clearer to read.
-       The reason is because a tuple can't unpack directly from a function return
+       Additionally it returns a string stating which number it lied about so
+       it can be called later.  This function return both objects in a list
+       instead of a tuple to cut back on code lines and to make it clearer to
+       read.  The reason is because a tuple can't unpack directly from
+       a function return
        """
 
     if guess == randomNumber:
-        return [False, "You guessed right when I was about to lie!"]
+        return "You guessed right when I was about to lie!"
     elif guess > randomNumber:
-        print(guess, "is too low  - please guess again" , end="")
+        print(guess, "is too low  - please guess again", end="")
         string = "I lied about " + str(guess) + " being too low"
-        return [True, string]
+        return string
     else:
         print(guess, "is too high - please guess again", end="")
         string = "I lied about " + str(guess) + " being too high"
-        return [True, string]
+        return string
+
 
 def shouldlie(percent):
-    """A function that returns true or false based on a hard coded percentage.  
+    """A function that returns true or false based on a hard coded percentage.
        The function determines if it is a float or an INT, to solve for
        the possible ways percentages might be wrote
        EX: 0.3 will have a 30% chance of returning true
        EX: 30 will have a 30% chance of returning true
        """
+
     if type(percent) == float:
         return percent > random.random()
     elif type(percent) == int:
-        return percent > random.randint(1,100)
+        return percent > random.randint(1, 100)
+
 
 def main():
 
-    randomNumber = random.randint(1,100)
+    randomNumber = random.randint(1, 100)
     guessAttempts = 0
     continueLoop = True
     haveLied = False
+    guess = ""
 
     whenILied = "I didn't lie at all"
     invalidInput = "is an invalid input, please type in a number 1-100"
 
     print("I'm thinking of a number from 1 to 100")
     print("Try and guess my number", end="")
+    print("DEBUG: The random number is", randomNumber)
 
-    while continueLoop:
+    while guess != randomNumber:
         try:
             guess = input(": ")
 
-            #Gives a valid way for user to exit program
+            # Gives a valid way for user to exit program
             if guess == "end":
                 exit()
-            #Checks for empty value
+            # Checks for empty value
             if guess == "":
-                guess = "Not entering a value"  #Corrects returned error
+                guess = "Not entering a value"  # Corrects returned error
                 raise ValueError
-            #Raises an error if value has a leading 0 or decimal
+            # Raises an error if value has a leading 0 or decimal
             if guess != str(int(guess)):
                 raise ValueError
-            #Checks for value outside of range
+            # Checks for value outside of range
             guess = int(guess)
             if (guess < 1) or (guess > 100):
                 raise ValueError
 
             guessAttempts += 1
-            #Determines if program should lie based on percent
+            # Determines if program should lie based on percent
             if (shouldlie(.25) and haveLied == False):
-                continueLoop, whenILied = comparenumberslie(guess, randomNumber)
+                whenILied = comparenumberslie(guess, randomNumber)
                 haveLied = True
             else:
-                #Assigns continueLoop False if player wins
-                continueLoop = comparenumbers(guess, randomNumber)
+                # Assigns continueLoop False if player wins
+                comparenumbers(guess, randomNumber)
 
-        #Handles all the exceptions in the program
+        # Handles all the exceptions in the program
         except ValueError:
             print(guess, invalidInput, end="")
- 
-    print(guess, "is correct!  You guessed my number in", guessAttempts, "guesses.")
+
+    print(guess, "is correct!  You guessed my number in",
+          guessAttempts, "guesses.")
     print(whenILied)
 
-             
+
 if __name__ == "__main__": main()
