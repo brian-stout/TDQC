fe82a8a05d3c97bc8f89ad2f0c4eb711f16d2c11
Brian Stout <brianjonstout@gmail.com>
2016-10-14 11:26:17 -0400

    Added a bunch of exception handling, commented some stuff and labeled debug lines. TODO: Implemented a weighted bool choice to determine when program lies

--- guessing/bstout_guessing.py
+++ guessing/bstout_guessing.py
@@ -2,7 +2,33 @@
 
 import random
 
+def comparenumbers(guess, randomNumber):
+    """This function compares two values, determines if it is equivalent,
+       greater than, or less than.  It then prints a string letting the
+       player know which it is.
+       
+       It returns a bool value of False if the values are equivalent which
+       tells the program to stop running the loop because the player won
+       """
+
+    if guess == randomNumber:
+        return False
+    elif guess > randomNumber:
+        print(guess, "is too high - please guess again", end="")
+        return True
+    elif guess < randomNumber:
+        print(guess, "is too low - please guess again", end="")
+        return True
+
 def comparenumberslie(guess, randomNumber):
+    """This function does the same thing as comparenumbers() but lies about the results
+
+       Additionally it returns a string stating which number it lied about so it
+       can be called later.  This function return both objects in a list instead
+       of a tuple to cut back on code lines and to make it clearer to read.
+       The reason is because a tuple can't unpack directly from a function return
+       """
+
     if guess == randomNumber:
         return [False, "You guessed right when I was about to lie!"]
     elif guess > randomNumber:
@@ -14,17 +40,16 @@ def comparenumberslie(guess, randomNumber):
         string = "I lied about " + str(guess) + " being too high"
         return [True, string]
 
-def comparenumbers(guess, randomNumber):
-    if guess == randomNumber:
-        return False
-    elif guess > randomNumber:
-        print(guess, "is too high - please guess again", end="")
-        return True
-    elif guess < randomNumber:
-        print(guess, "is too low - please guess again", end="")
-        return True
-
 def main():
+
+#TODO: Remove lieOnGuess debug value
+#TODO: Remove the debug print statements
+#TODO: Create a weighted random choice between True and False for the random lie attempt
+#TODO: Change the guess INT conversion to handle strings exclusively so that Python can
+#      determine when exceptions should happen
+#TODO: PEP-8 Compliance
+#TODO: Comment more stuff
+#TODO: Raise an error when user inputs number not between 0-101
 
     randomNumber = random.randint(1,100)
     guessAttempts = 0
@@ -40,21 +65,42 @@ def main():
 
     while continueLoop:
         try:
-            guess = int(input(": "))
+            guess = input(": ")
+
+            #Gives a valid way for user to exit program
+            if guess == "end":
+                exit()
+            guess = int(guess)
+
+            #Raises an error if the value is not from 1 to 100
+            if (guess < 1) or (guess > 100):
+                raise ValueError
             guessAttempts += 1
+
+            #Debug lines
             print("Your guess:", guess)
-            print("The number of guess currently", guessAttempts)
-            print("The program will lie on attempt #:", lieOnGuess)
+            print("Debug: The number of guess currently", guessAttempts)
+            print("Debug: The program will lie on attempt #:", lieOnGuess)
+
+            #Determines if the program should lie on this guess attempt
+            #Runs normally if it's not a lie, returns bool value to continue loop
+            #To determine if user has won and the loop should stop.
             if guessAttempts == lieOnGuess:
                 continueLoop, whenILied = comparenumberslie(guess, randomNumber)
             else:
                 continueLoop = comparenumbers(guess, randomNumber)
+
         except ValueError:
             print("Illegal value, please type in a number 1-100", end="")
+        except EOFError:
+            print(" Type end to terminate the program correctly")
+        except KeyboardInterrupt:
+            print(" Type end to terminate the program correctly")
+        except TypeError:
+            print("Illegal characters, please type in a number 1-100", end="")
     
     print(guess, "is correct!  You guessed my number in", guessAttempts, "guesses.")
     print(whenILied)
 
              
-
 if __name__ == "__main__": main()
