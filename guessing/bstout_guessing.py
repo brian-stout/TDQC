5f92e8e68fdc9245a71814ea16f8fb30840b9f0c
Brian Stout <brianjonstout@gmail.com>
2016-10-14 09:54:14 -0400

    Added the main logic to a fucntion, added error checking

--- guessing/bstout_guessing.py
+++ guessing/bstout_guessing.py
@@ -2,26 +2,36 @@
 
 import random
 
+def comparenumbers(guess, randomNumber):
+    if guess == randomNumber:
+        return False
+    elif guess > randomNumber:
+        print(guess, "is too high - please guess again", end="")
+        return True
+    elif guess < randomNumber:
+        print(guess, "is too low - please guess again", end="")
+        return True
+
 def main():
 
     randomNumber = random.randint(1,100)
     guessAttempts = 0
+    continueLoop = True
 
     print("Debug:  The random number is:", randomNumber, "\n\n\n")
     print("I'm thinking of a number from 1 to 100")
-    guess = int(input("Try and guess my number: "))
-    while True:
-        if guess == randomNumber:
-            print(guess, "is correct! You guessed my number in", guessAttempts)
-            break
-        elif guess > randomNumber:
-            print(guess, "is too high - please guess again", end="")
+    print("Try and guess my number", end="")
+
+    while continueLoop:
+        try:
+            guess = int(input(": "))
+            continueLoop = comparenumbers(guess, randomNumber)
             guessAttempts += 1
-        elif guess < randomNumber:
-            print(guess, "is too low - please guess again", end="")
-            guessAttempts += 1
-        guess = int(input(": "))
-        
-        
+        except ValueError:
+            print("Illegal value, please type in a number 1-100", end="")
+    
+    print(guess, "is correct!  You guessed my number in", guessAttempts)
+
+             
 
 if __name__ == "__main__": main()
